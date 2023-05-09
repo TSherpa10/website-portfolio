51e2fce30e30d5d3cab7b381e482dabe6379062b
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2023-05-09 13:54:25 -0400

    finished main-1 landing page

--- src/Landing.js
+++ src/Landing.js
@@ -1,20 +1,30 @@
 import "./Landing.css";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import React from "react";
+import Typewriter from "typewriter-effect";
 
 function Landing() {
   function btnLogicOne() {
-    console.log("placeholder-1");
+    console.log("loading pop-up screen of resume...");
   }
   function btnLogicTwo() {
-    console.log("placeholder-2");
+    window.location.href = 'https://github.com/TSherpa10';
+    console.log("navigating to github profile...");
   }
   function btnLogicThree() {
-    console.log("placeholder-3");
-  }
+    window.location.href = 'https://www.linkedin.com/in/tsherpa10/';
+    console.log("navigating to linkedin profile...");
+  };
   return (
     <>
       <div className="main">
         <article className="main-block-1">
-          <h1 id="main-header">TASHI SHERPA</h1>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString("TASHI SHERPA").start();
+            }}
+          />
+          <p className="main-text">my portfolio.</p>
           <div className="main-buttons">
             <button id="btn-1" onClick={btnLogicOne}>
               Resume
@@ -26,6 +36,13 @@ function Landing() {
               Linkedin
             </button>
           </div>
+            <h1 className="main-projects">projects</h1>
+            <ArrowDownwardIcon
+              className="arrowLogo"
+              sx={{
+                fontSize: 120,
+              }}
+            />
         </article>
         <article className="main-block-2"></article>
       </div>
